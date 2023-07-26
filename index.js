1965f468bc3bfdfe6a63bb274205e93a593a5fe6
Martin Malmheden <martin@pluie.se>
2023-07-26 22:04:43 +0200

    Less logging

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 8080 });
 let multiplier = 100;
 wss.on('connection', function connection(ws) {
-
+    console.log((new Date()).toLocaleDateString() + ' '+ (new Date()).toLocaleTimeString() + ' - Client connected')
     const useInterval = (m) => {
 
         if(ws){
@@ -14,17 +14,20 @@ wss.on('connection', function connection(ws) {
             const val2 = rand1 + Number(m)-250;
             const data= t/1000+',0,0,' + val1 +',0,0,0,' + val2;
             ws.send(data);
-            console.log((new Date()).toLocaleDateString() + ' '+ (new Date()).toLocaleTimeString() + ' ' + data)
+            //console.log((new Date()).toLocaleDateString() + ' '+ (new Date()).toLocaleTimeString() + ' ' + data)
         }
     };
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    //console.log('received: %s', data);
     let message = JSON.parse(data);
     multiplier = message.multiplier;
   });
 
-  ws.on('close', () => {clearInterval(interval)})
+  ws.on('close', () => {
+    clearInterval(interval);
+    console.log((new Date()).toLocaleDateString() + ' '+ (new Date()).toLocaleTimeString() + ' - Client disconnected')
+  });
 
   let interval = setInterval(() => useInterval(multiplier), 1000)
 });
